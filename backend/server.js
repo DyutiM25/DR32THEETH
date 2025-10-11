3023034b8668dde8e47fa454c5ee14ab1e5aa30f
Dyuti Mengji <mengjidyuti@gmail.com>
2025-10-11 01:02:35 -0700

    Code ready for Production : package.json, build & start commands, dist folder, and other relavant changes made

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path'; 
 
 import authRoutes from './routes/authRoutes.js';
 import appointmentRoutes from './routes/appointmentRoutes.js';
@@ -14,23 +15,34 @@ connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 
 app.use(express.json());
-app.use(cors({
-  origin: true, // your frontend URL (React dev server)
+if(process.env.NODE_ENV === 'development'){
+  app.use(cors({
+  origin: "http://localhost:5173", // your frontend URL (React dev server)
   credentials: true, // ✅ allow cookies (accessToken/refreshToken)
 }));
+}
 app.use(morgan('dev'));
 app.use(cookieParser());
 
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-  });
+// app.get('/', (req, res) => {
+//     res.send('API is running...');
+//   });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/appointments', appointmentRoutes);
+
+if (process.env.NODE_ENV === 'production') {  
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});  
+}
 
 
 app.listen(port, () => {
